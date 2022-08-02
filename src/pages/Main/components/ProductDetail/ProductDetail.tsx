37989514aec152c099d5002d59f4d139bf453ccb
Oleg Sheriff <olegashepelev@gmail.com>
2022-08-02 22:07:12 +0600

    feat: Add deatils for products

---
+++ src/pages/Main/components/ProductDetail/ProductDetail.tsx
@@ -0,0 +1,52 @@
+import './ProductDetail.scss';
+
+import React, { useEffect } from 'react';
+
+import { Col, Row } from 'antd';
+
+import GoBack from '../../../../components/Buttons/GoBack/GoBack';
+import { useActions } from '../../../../hooks/useAction';
+import { useTypedSelectorHook } from '../../../../hooks/useTypedSelector';
+import ProductDetailDescription from './components/ProductDetailDescription/ProductDetailDescription';
+import ProductDetailGallery from './components/ProductDetailGallery/ProductDetailGallery';
+
+const ProductDetail: React.FC = (props: any) => {
+  const idCurrentProduct = props.match.params.id;
+  const { currentProduct } = useTypedSelectorHook(state => state.products);
+  const { fetchCurrentProduct } = useActions();
+
+  useEffect(() => {
+    fetchCurrentProduct(idCurrentProduct);
+  }, [idCurrentProduct]);
+
+  if (Object.keys(currentProduct).length === 0) return <></>;
+
+  return (
+    <div className='container'>
+      <div className='wrapper-top product-wrap'>
+        <Row className='product__detail-row'>
+          <Col className='product__detail-col' flex='45%'>
+            <ProductDetailDescription
+              title={currentProduct.title}
+              price={currentProduct.price}
+              description={currentProduct.description}
+              category={currentProduct.categoryId?.title}
+              season={currentProduct.season}
+              roominess={currentProduct.roominess}
+              factory={currentProduct.factoryId?.title}
+              productId={idCurrentProduct}
+            />
+          </Col>
+          <Col className='product__detail-img' flex='55%'>
+            <ProductDetailGallery images={currentProduct.image ? currentProduct.image : []} />
+          </Col>
+        </Row>
+      </div>
+      <div style={{ margin: '0 0 10px 0' }}>
+        <GoBack />
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;
